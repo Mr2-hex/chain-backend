362a865f77a5f00ab40cc891b2c5baba13987e2a
Mr2-hex <onwohemmanuel96@gmail.com>
2025-10-01 03:35:40 +0100

    Changed Base Urls

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://chainarena-ten.vercel.app/",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
